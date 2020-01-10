61eea7545853d0150b5d701b53a8b74df922049e
Arkadramus <alexis.mignucci@gmail.com>
2020-01-10 15:16:04 +0100

    premier commit

---
+++ Epnos_application/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Epnos_application.Global" Language="C#" %>
---
+++ Epnos_application/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Epnos_application.Startup))]
+namespace Epnos_application
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
