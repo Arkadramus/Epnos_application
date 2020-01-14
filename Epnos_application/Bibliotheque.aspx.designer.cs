f8f51082b83b0058270768cbfe086fd9d8df2f87
Maurines <maurine.santigny@hotmail.fr>
2020-01-14 08:37:38 +0100

    Design de la page bibliothèque

--- Epnos_application/Bibliotheque.aspx.designer.cs
+++ Epnos_application/Bibliotheque.aspx.designer.cs
@@ -20,5 +20,32 @@ namespace Epnos_application {
         /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Contrôle btn_Recherche.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Recherche;
+        
+        /// <summary>
+        /// Contrôle btn_RetrouveRapport.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_RetrouveRapport;
+        
+        /// <summary>
+        /// Contrôle btn_Ouvrir.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Ouvrir;
     }
 }
